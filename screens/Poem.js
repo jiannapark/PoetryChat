929faaf5a2fa8e893170123cb943f1e6760fb360
Jianna Park <jiannajihyunpark@gmail.com>
2020-08-16 22:38:00 -0400

    add add title/save button

--- screens/Poem.js
+++ screens/Poem.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
 
 export default function Poem(props) {
+  const [title, onChangeText] = React.useState('Untitled');
+
   const text = props.navigation.state.params._55;
   const textArr = text.split(' ');
   let poem = [];
-  for (let i = 0; i < textArr.length; i++) {
+  for (let i = 0; i < 50; i++) {
+    // i < textArr.length
     if (i % 6 === 0) {
       poem.push('\n');
     }
@@ -16,6 +19,20 @@ export default function Poem(props) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{poem}</Text>
+      <View style={styles.save}>
+        <Text style={styles.prompt}>Save this poem?</Text>
+        <TextInput
+          style={styles.formInput}
+          onChangeText={text => onChangeText(text)}
+          placeholder="Untitled"
+          value={title}
+        />
+        <Button
+          onPress={() => props.handleSubmit()} // create this method
+          buttonStyle={styles.button}
+          title="Save"
+        />
+      </View>
     </View>
   );
 }
@@ -25,8 +42,28 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: {
+    flex: 1,
     padding: 20,
     fontSize: 18,
     lineHeight: 30,
+  },
+  save: {
+    paddingBottom: 50,
+    backgroundColor: 'pink',
+  },
+  prompt: {
+    padding: 20,
+    fontSize: 16,
+    color: 'gray',
+  },
+  titleInput: {
+    padding: 20,
+    width: '200px',
+    backgroundColor: 'pink',
+  },
+  button: {
+    backgroundColor: 'blue',
+    width: '200px',
+    marginTop: 100,
   },
 });
