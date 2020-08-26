ef7265a8f42ab4107007ddd19f8b631d085137a3
Jianna Park <jiannajihyunpark@gmail.com>
2020-08-25 22:07:59 -0400

    feat: add RoomList screen

---
+++ screens/RoomList.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+// import {getRooms} from '../firebase';
+
+const DATA = [
+  {
+    id: '',
+    title: 'Room 1',
+  },
+  {
+    id: '',
+    title: 'Room 2',
+  },
+  {
+    id: '',
+    title: 'Room 3',
+  },
+];
+
+const Item = ({item, onPress}) => (
+  <View style={styles.item}>
+    <TouchableOpacity onPress={onPress} style={styles.touchable}>
+      <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+export default function RoomList(props) {
+  // const [userId] = props.navigation.state.params;
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({item}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    return (
+      <Item
+        title={item.title}
+        onPress={() => setSelectedId(item.id)}
+        style={{backgroundColor}}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  touchable: {
+    backgroundColor: 'pink',
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 30,
+  },
+});
